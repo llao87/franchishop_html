c45e41e91ef6cb1a29d218f7fa181d6289e97f55
Дмитрий Крагель <di@MacBook-Pro-Dmitrij.local>
2022-08-05 18:45:02 +0300

    complete filter block, complete top franchises block, half of investments block

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('scss', async function() {
 gulp.task('css', async function() {
     return gulp.src([
             'node_modules/normalize.css/normalize.css',
-            'node_modules/slick-carousel/slick/slick.css',
+            'node_modules/swiper/swiper-bundle.min.css',
         ])
         .pipe(concat('_libs.scss'))
         .pipe(gulp.dest('src/scss'))
@@ -54,7 +54,8 @@ gulp.task('script', async function() {
 
 gulp.task('js', async function() {
     return gulp.src([
-            'node_modules/slick-carousel/slick/slick.js'
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/swiper/swiper-bundle.min.js',
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
-$(function(){
-
-
-
-  
+jQuery(function(){
+    // $('.single-slider').jRange({
+    //     from: -2.0,
+    //     to: 2.0,
+    //     step: 0.5,
+    //     scale: ["до 500 тыс. руб."-2.0,-1.0,0.0,1.0,2.0],
+    //     // format: '%s',
+    //     width: 1000,
+    //     showLabels: true,
+    //     snap: true
+    // });
 });
